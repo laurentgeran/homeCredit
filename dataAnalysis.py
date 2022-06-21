21873abb3fb898c6cd0775bda5e3790579757549
Laurent Géran <lgeran@dial-once.com>
2022-06-21 04:23:23 +0200

    long

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -14,8 +14,8 @@ def loadData(table, id:int = -1, index = True):
         df = pd.read_json(resp.json(),orient ='records')
     return(df)
 
-def loadDataIndexes(table, gender, age, family, car, id:int = -1, index = True):
-    url = NGROK_URL+"/dataIndex?table="+table+"&id="+str(id)+"&gender="+str(gender)+"&age="+str(age)+"&family="+str(family)+"&car="+str(car)
+def loadDataIndexes(table, gender, family, id:int = -1, index = True):
+    url = NGROK_URL+"/dataIndex?table="+table+"&id="+str(id)+"&gender="+str(gender)+"&family="+str(family)
     resp = requests.get(url)
     if index : 
         df = pd.read_json(resp.json(),orient ='records').set_index('index')
